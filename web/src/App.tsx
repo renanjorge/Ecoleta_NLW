330946c975af95460941d2d93374ae7aaf01252a
Renan8 <rh.azevedo3@gmail.com>
2020-06-06 14:53:38 -0300

    Cria web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+// JSX: Escrever sintaxe XML (HTML) no Javascript
+
+function App() {
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
